f5be522e9dd7b8104e4175ffc1f2309ae5f470db
Parker Erickson <parker.erickson@tigergraph.com>
2022-01-05 11:11:22 -0600

    update env

--- create_notebook.sh
+++ create_notebook.sh
@@ -2,24 +2,18 @@
 
 set -e
 
-# OVERVIEW
-# This script creates and configures the env_tf210_p36 environment.
-
 sudo -u ec2-user -i <<EOF
 
 echo ". /home/ec2-user/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
 
 # Create custom conda environment
-conda create -f 
+conda env create -f https://raw.githubusercontent.com/parkererickson/tg-sagemaker-test/main/environment.yml
 
 # Activate our freshly created environment
-source /home/ec2-user/anaconda3/bin/activate env_tf210_p36
-
-# Install git-repository dependencies
-pip install -q git+https://github.com/tensorflow/examples.git
+source /home/ec2-user/anaconda3/bin/activate tigergraph-ml
 
 # Expose environment as kernel
-python -m ipykernel install --user --name env_tf210_p36 --display-name My_Env_tf_2.1.0_py_3.6
+python -m ipykernel install --user --name tigergraph-ml --display-name TigerGraph-ML
 
 # Deactivate environment
 source /home/ec2-user/anaconda3/bin/deactivate
